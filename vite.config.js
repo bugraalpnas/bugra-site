03d66bab9435c3927893402adc763dab73fecb03
bugraalpnas <bugraalpnas@gmail.com>
2025-12-27 15:50:51 +0300

    Fix: Move admin to public and ensure config.yml is tracked

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         posts: resolve(__dirname, 'posts.html'),
-        admin: resolve(__dirname, 'admin/index.html'),
+
       },
     },
   },
